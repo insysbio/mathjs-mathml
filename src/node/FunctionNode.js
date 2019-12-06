3c45403356f2cba4c30e0365f85bd4630d7e5bc3
Evgeny Metelkin <metelkin@insysbio.com>
2019-12-06 11:16:28 +0300

    support log, cube, square functions

--- src/node/FunctionNode.js
+++ src/node/FunctionNode.js
@@ -5,8 +5,17 @@ exports.path = 'expression.node.FunctionNode.prototype';
 exports.factory = function() {
   return function() {
    let args = this.args
-     .map((arg) => arg.toCMathMLNode())
-     .join('');
-   return `<apply><${dictFunc[this.fn.name] || this.fn.name}/>${args}</apply>`;
+     .map((arg) => arg.toCMathMLNode());
+    if(this.fn.name==='cube'){
+      return `<apply><power/>${args[0]}<cn>3</cn></apply>`;
+    }else if(this.fn.name==='square'){
+      return `<apply><power/>${args[0]}<cn>2</cn></apply>`;
+    }else if(this.fn.name==='log' && this.args.length===2){
+      return `<apply><log/><logbase>${args[1]}</logbase>${args[0]}</apply>`;
+    }else if(this.fn.name==='log2'){
+      return `<apply><log/><logbase><cn>2</cn></logbase>${args[0]}</apply>`;
+    }else{ // change only function name
+      return `<apply><${dictFunc[this.fn.name] || this.fn.name}/>${args.join('')}</apply>`;
+    }
   };
 };
