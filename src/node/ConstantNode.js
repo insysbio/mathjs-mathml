a29ef031c12b9a84d9acc78b1445ac1cf6d27800
Evgeny Metelkin <metelkin@insysbio.com>
2021-03-05 14:58:23 +0300

    support boolean ConstantNode

--- src/node/ConstantNode.js
+++ src/node/ConstantNode.js
@@ -2,11 +2,14 @@ exports.name = 'toCMathMLNode';
 exports.path = 'expression.node.ConstantNode.prototype';
 exports.factory =  function() {
   return function(csymbols = {}) {
-    let check = String(this.value) // if it is exponential form
+    let isExponential = String(this.value) // if it is exponential form
       .match(/^[\d]+[.]?[\d]*[e][+-][\d]+$/);
-    if(check) {
+    let isBoolean = typeof this.value === 'boolean';
+    if (isExponential) {
       var value = String(this.value).match(/^([\d]+[.]?[\d]*)[e]([+-][\d]+)$/);
       return `<cn type="e-notation">${value[1]}<sep/>${value[2]}</cn>`;
+    } else if (isBoolean) {
+      return `<${this.value}/>`
     } else {
       return `<cn>${this.value}</cn>`;
     }
